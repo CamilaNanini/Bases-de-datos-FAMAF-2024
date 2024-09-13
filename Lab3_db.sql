9ece36b5af8830ef25dfca7f10690c77445ec04c
visitante <visitante@lab24.famaf.unc.edu.ar>
2024-09-13 16:14:21 -0300

    Ultimo punto a medio camino

--- Lab3_db.sql
+++ Lab3_db.sql
@@ -1,4 +1,4 @@
--- Listar el nombre de la ciudad y el nombre del país de todas las ciudades que pertenezcan a países con una población menor a 10000 habitantes.
+-- 1 Listar el nombre de la ciudad y el nombre del país de todas las ciudades que pertenezcan a países con una población menor a 10000 habitantes.
 SELECT city.Name, country.Name 
 FROM city INNER JOIN country
 ON city.CountryCode = country.Code 
@@ -8,7 +8,7 @@ WHERE country.Code IN (
 		WHERE country.Population < 10000
 );
 
--- Listar todas aquellas ciudades cuya población sea mayor que la población promedio entre todas las ciudades.
+-- 2 Listar todas aquellas ciudades cuya población sea mayor que la población promedio entre todas las ciudades.
 SELECT city.Name 
 FROM city 
 WHERE city.Population > ALL(
@@ -16,7 +16,7 @@ WHERE city.Population > ALL(
 			FROM city
 );
 
--- Listar todas aquellas ciudades no asiáticas cuya población sea igual o mayor a la población total de algún país de Asia.
+-- 3 Listar todas aquellas ciudades no asiáticas cuya población sea igual o mayor a la población total de algún país de Asia.
 SELECT city.Name
 FROM city INNER JOIN country
 ON city.CountryCode = country.Code 
@@ -27,7 +27,7 @@ WHERE country.Continent != 'Asia' AND city.Population >= SOME(
 				WHERE country.Continent = 'Asia'
 );
 
--- Listar aquellos países junto a sus idiomas no oficiales, que superen en porcentaje de hablantes a cada uno de los idiomas oficiales del país.
+-- 4 Listar aquellos países junto a sus idiomas no oficiales, que superen en porcentaje de hablantes a cada uno de los idiomas oficiales del país.
 SELECT country.Name , countrylanguage.Language 
 FROM country INNER JOIN countrylanguage
 ON country.Code = countrylanguage.CountryCode AND countrylanguage.IsOfficial != 'T'
@@ -36,7 +36,7 @@ WHERE countrylanguage.Percentage > ALL (
         FROM countrylanguage
         WHERE countrylanguage.IsOfficial = 'T' AND country.Code = countrylanguage.CountryCode
 );
--- Listar (sin duplicados) aquellas regiones que tengan países con una superficie menor a 1000 km2 y exista (en el país) al menos una ciudad con más de 100000 habitantes. 
+-- 5 Listar (sin duplicados) aquellas regiones que tengan países con una superficie menor a 1000 km2 y exista (en el país) al menos una ciudad con más de 100000 habitantes. 
 -- Con subquery
 SELECT DISTINCT country.Region 
 FROM country 
@@ -51,7 +51,7 @@ FROM country INNER JOIN city
 ON city.CountryCode = country.Code 
 WHERE country.SurfaceArea < 1000 AND 100000 < city.Population
 
--- Listar el nombre de cada país con la cantidad de habitantes de su ciudad más poblada. 
+-- 6 Listar el nombre de cada país con la cantidad de habitantes de su ciudad más poblada. 
 -- (Hint: Hay dos maneras de llegar al mismo resultado. Usando consultas escalares o usando agrupaciones, encontrar ambas).
 -- Consulta escalar
 SELECT DISTINCT country.name, (SELECT MAX(city.population)
@@ -63,9 +63,9 @@ FROM country;
 SELECT country.Name, max(city.Population)
 FROM country INNER JOIN city 
 ON country.Code = city.CountryCode
-GROUP BY city.CountryCode ;
+GROUP BY country.Code ;
 
--- Listar aquellos países y sus lenguajes no oficiales cuyo porcentaje de hablantes sea mayor al promedio de hablantes de los lenguajes oficiales. 
+-- 7 Listar aquellos países y sus lenguajes no oficiales cuyo porcentaje de hablantes sea mayor al promedio de hablantes de los lenguajes oficiales. 
 SELECT country.Name , countrylanguage.Language
 FROM country INNER JOIN countrylanguage
 ON country.Code = countrylanguage.CountryCode AND countrylanguage.IsOfficial!='T'
@@ -74,13 +74,13 @@ WHERE countrylanguage.Percentage > ALL(
 			FROM countrylanguage
 			WHERE countrylanguage.IsOfficial = 'T' AND country.Code = countrylanguage.CountryCode 
 )
--- Listar la cantidad de habitantes por continente ordenado en forma descendente.
+-- 8 Listar la cantidad de habitantes por continente ordenado en forma descendente.
 SELECT country.Continent , SUM(country.Population) as cpopulation
 FROM country 
 GROUP BY country.Continent
 ORDER BY cpopulation DESC;
 
--- Listar el promedio de esperanza de vida (LifeExpectancy) por continente con una esperanza de vida entre 40 y 70 años.
+-- 9 Listar el promedio de esperanza de vida (LifeExpectancy) por continente con una esperanza de vida entre 40 y 70 años.
 SELECT country.Continent , AVG(country.LifeExpectancy) AS clifeexpextanxy
 FROM country 
 GROUP BY country.Continent
@@ -91,3 +91,29 @@ ORDER BY clifeexpextanxy DESC
 SELECT  country.continent, MAX(country.Population), MIN(country.Population), AVG(country.Population), SUM(country.Population)
 FROM country
 GROUP BY country.Continent;
+
+-- Modificar la consulta 6 para que además devuelva el nombre de la ciudad más poblada. 
+-- ¿Podría lograrse con agrupaciones? ¿y con una subquery escalar?
+SELECT country.Name,city.Name,city.Population
+FROM country INNER JOIN city 
+ON country.Code = city.CountryCode
+WHERE city.Population = ALL(
+		SELECT MAX(city.Population)
+		FROM city 
+		WHERE city.CountryCode = country.Code
+		);
+		
+-- SELECT c.Name AS CountryName, ci.Name AS CityName, ci.Population
+-- FROM country c
+-- INNER JOIN city ci
+--     ON c.Code = ci.CountryCode
+-- INNER JOIN (
+--     SELECT CountryCode, MAX(Population) AS MaxPopulation
+--     FROM city
+--     GROUP BY CountryCode
+-- ) max_pop
+--     ON ci.CountryCode = max_pop.CountryCode
+--     AND ci.Population = max_pop.MaxPopulation;
+
+
+
